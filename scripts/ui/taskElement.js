33af88324b2a71257fe8015178fc8d34ea637861
Ayabonga Mtsotso <ayabonga.afika.mtsotso@gmail.com>
2025-09-27 17:30:38 +0200

    Adds priority sorting

--- scripts/ui/taskElement.js
+++ scripts/ui/taskElement.js
@@ -1,17 +1,33 @@
 import { openTaskModal } from "./modalHandlers.js";
 /**
  * Creates a task DOM element.
- * @param {*} task The task object.
- * @returns {HTMLElement} The task element.
+ * @param {*} task 
+ * @returns
  */
 export function createTaskElement(task) {
   const taskDiv = document.createElement("div");
   taskDiv.className = "task-div";
-  taskDiv.textContent = task.title;
   taskDiv.dataset.taskId = task.id;
 
+const titleDiv = document.createElement("div");
+  titleDiv.classList.add("task-title");
+
+  // Priority dot
+  const dot = document.createElement("span");
+  dot.classList.add("priority-dot", `priority-${task.priority?.toLowerCase() || "low"}`);
+
+  // Title text
+  const titleText = document.createElement("span");
+  titleText.textContent = task.title;
+
+  
+  titleDiv.appendChild(dot);
+  titleDiv.appendChild(titleText);
+
+  taskDiv.appendChild(titleDiv);
+
   taskDiv.addEventListener("click", () => {
-    openTaskModal(task);
+    openTaskModal(task); 
   });
 
   return taskDiv;
